49293ed8a0950c77f25a0eb62c6da640afcf66a7
software-mha <software.mha.ali@gmail.com>
2024-10-23 12:49:21 +0500

    initial setup

---
+++ main.js
@@ -0,0 +1,42 @@
+import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.querySelector("#webgl");
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    1000
+);
+camera.position.set(0, 0, 5);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+window.addEventListener("resize", () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
